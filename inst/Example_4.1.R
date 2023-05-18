d9d5362f9706e1b73d0513be11affa081483d8b4
TzWng <91708166+TzWng@users.noreply.github.com>
2023-05-17 21:08:15 -0400

    Update Example_4.1.R

--- inst/Example_4.1.R
+++ inst/Example_4.1.R
@@ -33,9 +33,9 @@ p = 0.7
 ## 0.6, 0.625, 0.65, 0.675, 0.7, 0.725,
 ## 0.75, 0.775, 0.8
 # An integer at which to start computing the unbiased estimator
-k <- 8000#3e5
+k <- 4e4
 # A time horizon: the chains are sampled until the maximum between m and the meeting time
-m <- 10000#4e5
+m <- 16e4
 ## nrep samples required
 nrep = 180
 n_calls = 2^(rgeom(nrep,p)+1)
@@ -51,5 +51,34 @@ for (i in 1:nrep){
 }
 
 
+
+p = 0.7
+# 0.6, 0.625, 0.65, 0.675, 0.7, 0.725,
+# 0.75, 0.775, 0.8
+# nrep samples required
+nrep = 105
+n_calls = 2^(rgeom(nrep,p)+1)
+Z = rep(0, nrep)
+m = 16e4
+for (i in 1:nrep){
+  current = rinit()
+  total = m*n_calls[i]
+  burnin = total*0.1
+  #chain = matrix(NA, ncol = d, nrow = total)
+  chain = rep(0,d)
+  for(imcmc in 1:total){
+    current <- single_kernel(current)
+    current_state <- current$chain_state
+    if(imcmc > burnin){
+      chain = chain + current_state
+    }
+  }
+  chain_mean = chain/(total - burnin)
+  # chain = chain[(burnin+1):total,]
+  # col_mean = apply(chain,2,mean)
+  # Z[i] = g(col_mean)
+  Z[i] = g(chain_mean)
+  print(Z[i])
+}
 
 
