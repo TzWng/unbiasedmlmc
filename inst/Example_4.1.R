9a967edac4d2ae1e2ed1ff2b9b07a909fc0d2e53
TzWng <91708166+TzWng@users.noreply.github.com>
2023-05-17 21:09:45 -0400

    Update Example_4.1.R

--- inst/Example_4.1.R
+++ inst/Example_4.1.R
@@ -37,7 +37,7 @@ k <- 4e4
 # A time horizon: the chains are sampled until the maximum between m and the meeting time
 m <- 16e4
 ## nrep samples required
-nrep = 180
+nrep = 50
 n_calls = 2^(rgeom(nrep,p)+1)
 Z = rep(0, nrep)
 H <- list()
@@ -51,12 +51,9 @@ for (i in 1:nrep){
 }
 
 
-
-p = 0.7
-# 0.6, 0.625, 0.65, 0.675, 0.7, 0.725,
-# 0.75, 0.775, 0.8
+### standard mcmc estimator with same cost as unbiased mcmc_mlmc
 # nrep samples required
-nrep = 105
+nrep = 50
 n_calls = 2^(rgeom(nrep,p)+1)
 Z = rep(0, nrep)
 m = 16e4
@@ -64,7 +61,6 @@ for (i in 1:nrep){
   current = rinit()
   total = m*n_calls[i]
   burnin = total*0.1
-  #chain = matrix(NA, ncol = d, nrow = total)
   chain = rep(0,d)
   for(imcmc in 1:total){
     current <- single_kernel(current)
@@ -74,9 +70,6 @@ for (i in 1:nrep){
     }
   }
   chain_mean = chain/(total - burnin)
-  # chain = chain[(burnin+1):total,]
-  # col_mean = apply(chain,2,mean)
-  # Z[i] = g(col_mean)
   Z[i] = g(chain_mean)
   print(Z[i])
 }
